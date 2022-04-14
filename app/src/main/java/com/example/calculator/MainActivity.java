5bee12698012cf7357547322a752d6ff504081f4
ProBook_26 <kkopol245@mail.ru>
2022-04-14 16:23:54 +0300

    ok

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     String lastOperation = "=";
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
